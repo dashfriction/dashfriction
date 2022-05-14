670387cd2ec780b34503269cb46ee854f9c9845c
Sean Knowles <s@seanknowles.dev>
2022-05-14 13:12:48 +0100

    feat: initial commit

---
+++ apps/client-web/src/App.tsx
@@ -0,0 +1,12 @@
+import { UIButton } from "ui";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UIButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui/button/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Button } from "@mantine/core";
+
+export const UIButton = () => {
+  return <Button>Settings</Button>;
+};
